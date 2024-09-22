11ae486f64e45f504b30dd2f0919f2a5980731b7
mk9973 <mantoshkk367@gmail.com>
2024-09-22 12:49:11 +0530

    WeatherApp added

--- bmiCalculator/src/App.jsx
+++ bmiCalculator/src/App.jsx
@@ -1,88 +1,100 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const[wt,setWt]=useState("");
-  const[ht,setHt]=useState("");
-  const[result,setResult]=useState("");
-  const[bmi,setBmi]=useState(false);
+  const [wt, setWt] = useState("");
+  const [ht, setHt] = useState("");
+  const [message, setMessage] = useState("");
+  const [bmi, setBmi] = useState("");
 
-  function onSubmitButton(){
-      setBmi(true);
-      const calculatedBMI = (wt / (ht * ht)) * 703;
-      setResult(calculatedBMI.toFixed(2));
-      console.log(calculatedBMI)
+  function onSubmitButton() {
+    if (wt === '0' || ht === '0' || wt === "" || ht === "") {
+      alert("Please provide valid input");
+    } else {
+      let calculatedBMI = (wt / (ht * ht)) * 703;
+      calculatedBMI = calculatedBMI.toFixed(1);
+      setBmi(calculatedBMI);
+
+      if (calculatedBMI < 25) {
+        setMessage("You are underweight");
+      } else if (calculatedBMI >= 25 && calculatedBMI < 30) {
+        setMessage("You are healthy");
+      } else {
+        setMessage("You are overweight");
+      }
+    }
   }
 
-  function onChangeHandlerW(e){
-      setWt(e.target.value);
+  function onChangeHandlerW(e) {
+    setWt(e.target.value);
   }
 
-  function onChangeHandlerH(e){
-      setHt(e.target.value)
+  function onChangeHandlerH(e) {
+    setHt(e.target.value);
   }
 
   return (
-    <div class="bg-gray-100 flex justify-center items-center h-screen">
-    <div class="w-full max-w-xs bg-white shadow-md rounded-lg p-8">
-      <h2 class="text-center text-xl font-bold mb-6">BMI Calculator</h2>
+    <div className="bg-gray-100 flex justify-center items-center h-screen">
+      <div className="w-full max-w-xs bg-white shadow-md rounded-lg p-8">
+        <h2 className="text-center text-xl font-bold mb-6">BMI Calculator</h2>
 
-      <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="weight">
-          Weight (lbs)
-        </label>
-        <input
-          type="number"
-          id="weight"
-          placeholder="Enter weight"
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          value={wt}
-          onChange={onChangeHandlerW}
-        />
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="weight">
+            Weight (lbs)
+          </label>
+          <input
+            type="number"
+            id="weight"
+            placeholder="Enter weight"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            value={wt}
+            onChange={onChangeHandlerW}
+          />
+        </div>
+
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="height">
+            Height (in)
+          </label>
+          <input
+            type="number"
+            id="height"
+            placeholder="Enter height"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            value={ht}
+            onChange={onChangeHandlerH}
+          />
+        </div>
+
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+            onClick={onSubmitButton}
+          >
+            Submit
+          </button>
+          <button
+            className="bg-gray-300 hover:bg-gray-400 text-gray-700 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+            onClick={() => {
+              setHt("");
+              setWt("");
+              setBmi("");
+              setMessage("");
+            }}
+          >
+            Reload
+          </button>
+        </div>
+
+        <p className="mt-6 text-center text-gray-700">
+          {bmi && <>Your BMI is: {bmi}</>}
+          <div>{message}</div>
+        </p>
       </div>
-
-      <div class="mb-6">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="height">
-          Height (in)
-        </label>
-        <input
-          type="number"
-          id="height"
-          placeholder="Enter height value"
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          value={ht}
-          onChange={onChangeHandlerH}
-        />
-      </div>
-
-      <div class="flex items-center justify-between">
-        <button
-          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          type="button"
-          onClick={onSubmitButton}
-        >
-          Submit
-        </button>
-        <button
-          class="bg-gray-300 hover:bg-gray-400 text-gray-700 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          type="button"
-          onClick={()=>{ setHt("")
-            setWt("")
-            setBmi(false);
-          }}
-        >
-          Reload
-        </button>
-      </div>
-
-      <p class="mt-6 text-center text-gray-700">
-        Your BMI is: {bmi ?<span class="font-bold">{result}</span> :<span class="font-bold">N/A</span>}
-      </p>
     </div>
-  </div>
   )
 }
 
-export default App
+export default App;
